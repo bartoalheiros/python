e245a61b630a078cfb77de89ba1e2da006a611a1
BartoAlheiros <alheirosb@gmail.com>
2024-01-28 10:30:14 -0300

    Continuando o exercício.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from pessoa import Pessoa
 
 p1 = Pessoa('Luiz', 29)
-p1.outro_metodo()
+p1.comer('maçã')
+p1.falar('POO')
+p1.parar_comer()
+p1.falar('POO')
--- pessoa.py
+++ pessoa.py
@@ -5,5 +5,30 @@ class Pessoa:
         self.comendo = comendo
         self.falando = falando
 
-    def outro_metodo(self):
-        print(self.nome)
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.nome} não pode falar comendo.')
+            return
+        
+        if self.falando:
+            print(f'{self.nome} já está falando.')
+            return
+        
+        print(f'{self.nome} está falando sobre {assunto}.')
+        self.falando = True
+
+    def comer(self, alimento):
+        if self.comendo:
+             print(f'{self.nome} já está comendo.')
+             return
+        
+        print(f'{self.nome} está comendo {alimento}.')
+        self.comendo = True
+
+    def parar_comer(self):
+        if not self.comendo:
+            print(f'{self.nome} não está comendo.')
+            return
+        
+        print(f'{self.nome} parou de comer.')
+        self.comendo = False
