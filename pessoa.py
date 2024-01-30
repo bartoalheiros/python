24b482ac1b9b976d702e1e47fd7bda45591c6f21
BartoAlheiros <alheirosb@gmail.com>
2024-01-30 10:32:09 -0300

    metodos estaticos

--- pessoa.py
+++ pessoa.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from random import randint
 
 class Pessoa:
     ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
@@ -56,3 +57,10 @@ class Pessoa:
     def por_ano_nascimento(cls, nome, ano_nascimento):
         idade = cls.ano_atual - ano_nascimento
         return cls(nome, idade)
+    
+    @staticmethod
+    def gera_id():
+        rand  = randint(10000, 19999)
+        return rand
+    
+    
