6e4e1e96df6e6ea1989e4a1094107a95d3a57a2a
BartoAlheiros <alheirosb@gmail.com>
2024-01-31 09:51:49 -0300

    modificadores de acesso

--- base_dados.py
+++ base_dados.py
@@ -1,25 +1,32 @@
 """
 public, protected, private
+_ privado/protected (public _)
+__ privado (_NOMECLASSE__nomeatributo)
 """
 class BaseDeDados:
     def __init__(self):
-        self._dados = {}
+        self.__dados = {}
+
+    @property
+    def dados(self):
+        return self.__dados
 
     def inserir_cliente(self, id, nome):
-        if 'clientes' not in self._dados:
-            self._dados['clientes'] = {id: nome}
+        if 'clientes' not in self.__dados:
+            self.__dados['clientes'] = {id: nome}
         else:
-            self._dados['clientes'].update({id: nome})
+            self.__dados['clientes'].update({id: nome})
 
     def lista_clientes(self):
-        for id, nome in self._dados['clientes'].items():
+        for id, nome in self.__dados['clientes'].items():
             print(id, nome)
 
     def apaga_cliente(self, id):
-        del self._dados['clientes'][id]
+        del self.__dados['clientes'][id]
 
 bd = BaseDeDados()
 bd.inserir_cliente(1, 'Otavio')
 bd.inserir_cliente(2, 'Rose')
-bd._dados = 'Outra coisa'
-bd.lista_clientes()              
+bd.__dados = 'Outra coisa'
+# bd.lista_clientes()              
+print(bd._BaseDeDados__dados)
