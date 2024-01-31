dfeecb74527d179a33a2f26f4aae6bce15030ebf
BartoAlheiros <alheirosb@gmail.com>
2024-01-31 16:42:49 -0300

    modularizando testes

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -6,16 +6,12 @@ def main():
 
 
 def test_square():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 squared was not 4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 squared was not 9")    
+    assert square(2) == 4
+    assert square(3) == 9
 
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
 
-
-if __name__ == "__main__":
-    main()
+def test_zero():
+    assert square(0) == 0
