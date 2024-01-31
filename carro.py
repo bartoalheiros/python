3fdd76f86fabc15ef84e329ce8b38c455d43d096
BartoAlheiros <alheirosb@gmail.com>
2024-01-31 14:25:42 -0300

    metodo getVelocidade adicionado

--- carro.py
+++ carro.py
@@ -12,12 +12,16 @@ class Carro:
 
     #modifica a velocidade
     def setVelocidade(self, novaVelocidade):
-        if novaVelocidade >= 0:
+        if novaVelocidade >= 0 and novaVelocidade < 50:
             self.__velocidade = novaVelocidade
 
+    def getVelocidade(self):
+        return self.__velocidade
+
 carro1 = Carro()
+print(f'velocidade atual: {carro1.getVelocidade()}')
 carro1.setVelocidade(10)
-print(f'velocidade atual: {carro1.velocidade}')
+print(f'velocidade atual: {carro1.getVelocidade()}')
 # print(carro1._Carro__velocidade)
 # carro1.__velocidade = 50
 # carro1.__velocidade = carro1.__velocidade * -1
