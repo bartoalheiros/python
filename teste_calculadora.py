361c946755000e31a800bae22b3ba480bf6909a0
BartoAlheiros <alheirosb@gmail.com>
2024-01-31 15:29:51 -0300

    testes unitarios sem pytest

---
+++ teste_calculadora.py
@@ -0,0 +1,21 @@
+from calculadora import square
+
+
+def main():
+    test_square()
+
+
+def test_square():
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 squared was not 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 squared was not 9")    
+
+
+
+if __name__ == "__main__":
+    main()
